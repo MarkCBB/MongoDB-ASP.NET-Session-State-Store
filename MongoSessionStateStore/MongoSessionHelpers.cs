b5bba151ddc124b4258f6ef1f895d6ff9a2cbeff
MarkCBB <mark@markcortada.com>
2015-03-15 17:40:14 +0100

    Refactored and documented static extension helpers

--- MongoSessionStateStore/MongoSessionHelpers.cs
+++ MongoSessionStateStore/MongoSessionHelpers.cs
@@ -1,16 +1,23 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
-using System.Web.Mvc;
+using System.Web;
 
 namespace MongoSessionStateStore.Helpers
 {
     public static class MongoSessionUserHelpers
     {
-        public static T GetMongoSession<T>(
-            this Controller obj,
+        /// <summary>
+        /// Gets the session value stored in MongoDB.
+        /// </summary>
+        /// <typeparam name="T">Type of the value to get.</typeparam>
+        /// <param name="session">HttpSessionStateBase object for MVC pages.</param>
+        /// <param name="key">The session key name.</param>
+        /// <returns>The object requested. Null if not exists.</returns>
+        public static T Mongo<T>(
+            this HttpSessionStateBase session,
             string key)
         {
-            var sessionObj = obj.Session[key];
+            var sessionObj = session[key];
             if (sessionObj == null)
                 return default(T);
 
@@ -26,12 +33,19 @@ namespace MongoSessionStateStore.Helpers
             return default(T);
         }
 
-        public static void SetMongoSession<T>(
-            this Controller obj,
+        /// <summary>
+        /// Sets the session value to store in MongoDB.
+        /// </summary>
+        /// <typeparam name="T">Type of value to store.</typeparam>
+        /// <param name="session">HttpSessionStateBase object for MVC pages.</param>
+        /// <param name="key">The session key name.</param>
+        /// <param name="newValue">The value to store.</param>
+        public static void Mongo<T>(
+            this HttpSessionStateBase session,
             string key,
-            T value)
+            T newValue)
         {
-            obj.Session[key] = value;
+            session[key] = newValue;
         }        
     }
 }
--- TestApplicationv2_0/Controllers/DefaultWithHelpersController.cs
+++ TestApplicationv2_0/Controllers/DefaultWithHelpersController.cs
@@ -19,20 +19,20 @@ namespace TestApplicationv2_0.Controllers
 
         public ActionResult Index()
         {
-            this.SetMongoSession<string>(KEY_NAME, "Hi");
+            Session.Mongo<string>(KEY_NAME, "Hi");
             return View();
         }
 
         public ActionResult GetAndSetSameRequest()
         {
             Person personSetted = new Person() { Name = "Marc", Surname = "Cortada", City = "Barcelona" };
-            this.SetMongoSession<Person>(KEY_NAME, personSetted);
+            Session.Mongo<Person>(KEY_NAME, personSetted);
             
             int settedValInt = 3;
-            this.SetMongoSession<int>(KEY_NAME2, settedValInt);            
+            Session.Mongo<int>(KEY_NAME2, settedValInt);
 
-            Person personGetted = this.GetMongoSession<Person>(KEY_NAME);
-            int gettedValInt = this.GetMongoSession<int>(KEY_NAME2);
+            Person personGetted = Session.Mongo<Person>(KEY_NAME);
+            int gettedValInt = Session.Mongo<int>(KEY_NAME2);
 
             if ((settedValInt == gettedValInt) && (personSetted == personGetted))
                 ViewBag.allOk = "True";
@@ -44,7 +44,7 @@ namespace TestApplicationv2_0.Controllers
 
         public ActionResult PrintSessionValString()
         {
-            string val = this.GetMongoSession<string>(KEY_NAME);
+            string val = Session.Mongo<string>(KEY_NAME);
 
             ViewBag.sessionVal = val;
             return View("~/Views/Default/PrintSessionVal.aspx");
@@ -52,7 +52,7 @@ namespace TestApplicationv2_0.Controllers
 
         public ActionResult PrintSessionValDouble()
         {
-            double dobVal = this.GetMongoSession<double>(KEY_NAME);
+            double dobVal = Session.Mongo<double>(KEY_NAME);
 
             ViewBag.sessionVal = dobVal.ToString("G");
             return View("~/Views/Default/PrintSessionVal.aspx");
@@ -60,14 +60,14 @@ namespace TestApplicationv2_0.Controllers
 
         public ActionResult SetSessionValInt(int newSesVal = 0)
         {
-            this.SetMongoSession<int>(KEY_NAME, newSesVal);
+            Session.Mongo<int>(KEY_NAME, newSesVal);
 
             return View("~/Views/Default/SetSessionVal.aspx");
         }
 
         public ActionResult SetSessionValBool(bool newSesVal = false)
         {
-            this.SetMongoSession<bool>(KEY_NAME, newSesVal);
+            Session.Mongo<bool>(KEY_NAME, newSesVal);
 
             return View("~/Views/Default/SetSessionVal.aspx");
         }
@@ -75,14 +75,14 @@ namespace TestApplicationv2_0.Controllers
         public ActionResult SetSessionValDouble()
         {
             double newSesVal = 3.1416F;
-            this.SetMongoSession<double>(KEY_NAME, newSesVal);
+            Session.Mongo<double>(KEY_NAME, newSesVal);
 
             return View("~/Views/Default/SetSessionVal.aspx");
         }
 
         public ActionResult SetSessionVal(string newSesVal = "")
         {
-            this.SetMongoSession<string>(KEY_NAME, newSesVal);
+            Session.Mongo<string>(KEY_NAME, newSesVal);
 
             return View();
         }
