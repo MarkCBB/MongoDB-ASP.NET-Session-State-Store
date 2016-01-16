fa61ef37318f0ab5fa33b2300ee5e5d692f3fce1
MarkCBB <mark@markcortada.com>
2016-01-16 19:12:06 +0100

    Added test project for RAW serialization and removed old project. issue #5

---
+++ TestApplication_RAWSerialization.Tests/PrimitiveTypes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace TestApplication_RAWSerialization.Tests
+{
+    [TestClass]
+    public class PrimitiveTypes
+    {
+        private string _controller = "Home";
+
+        [TestMethod]
+        public void TestInt()
+        {
+            CookieContainer cookie = new CookieContainer();
+            string url1 = string.Format(TestApplication_RAW_Helpers.BASE_URL,
+                _controller, "SetIntVal");
+            string url2 = string.Format(TestApplication_RAW_Helpers.BASE_URL,
+                _controller, "GetIntVal");
+            
+            string result = TestApplication_RAW_Helpers.DoRequest(url1, cookie);
+            string result2 = TestApplication_RAW_Helpers.DoRequest(url2, cookie);
+            StringAssert.Contains(result, "<sessionVal>3</sessionVal>");
+            StringAssert.Contains(result2, "<sessionVal>3</sessionVal>");
+        }
+
+        [TestMethod]
+        public void TestString()
+        {
+            CookieContainer cookie = new CookieContainer();
+            string url1 = string.Format(TestApplication_RAW_Helpers.BASE_URL,
+                _controller, "SetStringVal");
+            string url2 = string.Format(TestApplication_RAW_Helpers.BASE_URL,
+                _controller, "GetStringVal");
+
+            string result1 = TestApplication_RAW_Helpers.DoRequest(url1, cookie);
+            string result2 = TestApplication_RAW_Helpers.DoRequest(url2, cookie);
+            StringAssert.Contains(result1, "<sessionVal>Barcelona</sessionVal>");
+            StringAssert.Contains(result2, "<sessionVal>Barcelona</sessionVal>");
+        }
+
+        [TestMethod]
+        public void TestObjects()
+        {
+            CookieContainer cookie = new CookieContainer();
+            string url1 = string.Format(TestApplication_RAW_Helpers.BASE_URL,
+                _controller, "GetAndSetSameRequestObjects");
+            string url2 = string.Format(TestApplication_RAW_Helpers.BASE_URL,
+                _controller, "GetObjects");
+
+            string result1 = TestApplication_RAW_Helpers.DoRequest(url1, cookie);
+            string result2 = TestApplication_RAW_Helpers.DoRequest(url2, cookie);
+            StringAssert.Contains(result1, "<sessionVal>True</sessionVal>");
+            StringAssert.Contains(result2, "<sessionVal>True</sessionVal>");
+        }
+
+        [TestMethod]
+        public void TestObjectsWithOutType()
+        {
+            CookieContainer cookie = new CookieContainer();
+            string url1 = string.Format(TestApplication_RAW_Helpers.BASE_URL,
+                _controller, "GetAndSetSameRequestObjects");
+            string url2 = string.Format(TestApplication_RAW_Helpers.BASE_URL,
+                "GetWithoutType", "Index");
+
+            string result1 = TestApplication_RAW_Helpers.DoRequest(url1, cookie);
+            string result2 = TestApplication_RAW_Helpers.DoRequest(url2, cookie);
+            StringAssert.Contains(result1, "<sessionVal>True</sessionVal>");
+            StringAssert.Contains(result2, "<sessionVal>True</sessionVal>");
+        }
+    }
+}
---
+++ TestApplication_RAWSerialization.Tests/TestApplication_RAW_Helpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication_RAWSerialization.Tests
+{
+    internal static class TestApplication_RAW_Helpers
+    {
+        public static string BASE_URL = "http://localhost/TestApplication_RAWSerialization/{0}/{1}";
+
+        public static string DoRequest(
+            string url,
+            CookieContainer cookieContainer)
+        {
+            HttpWebRequest request =
+                (HttpWebRequest)WebRequest.Create(url);
+            request.CookieContainer = cookieContainer;
+            var stream = request.GetResponse().GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+}
